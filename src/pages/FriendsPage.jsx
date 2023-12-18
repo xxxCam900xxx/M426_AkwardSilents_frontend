2122c93f3130622941c0dee27e4f00f1039cb5da
LorenaVennemann <113357105+LorenaVennemann@users.noreply.github.com>
2023-12-18 16:16:31 +0100

    suchfrage friendpage

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Text, View, FlatList, TouchableOpacity, Modal, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { Text, View, FlatList, TouchableOpacity, Modal, StyleSheet, TextInput } from 'react-native';
 import { MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -50,88 +50,127 @@ const availableContacts = [
 ];
 
 function FriendsPage() {
-
     const navigation = useNavigation();
-
     const [isModalVisible, setModalVisible] = useState(false);
-
+    const [searchQuery, setSearchQuery] = useState('');
+    const searchInputRef = useRef(null);
+  
     const toggleModal = () => {
-        setModalVisible(!isModalVisible);
+      setModalVisible(!isModalVisible);
+      setSearchQuery(''); // Zurücksetzen der Suchanfrage beim Schließen des Modals
     };
-
+  
+    // Funktion zum Filtern von Chats und Kontakten basierend auf der Suchanfrage
+    const filterData = (data) => {
+      return data.filter(
+        (item) =>
+          (item.name && item.name.toLowerCase().includes(searchQuery.toLowerCase())) ||
+          (item.lastMessage && item.lastMessage.toLowerCase().includes(searchQuery.toLowerCase()))
+      );
+    };
+  
     return (
-        <View style={{ flex: 1, backgroundColor: '#232D3F' }}>
-            {/* Header mit Titel und Add Contact Button */}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, paddingTop: 51 }}>
-                <Text style={{ color: 'white', fontSize: 24 }}>Recent Chats</Text>
-                <TouchableOpacity style={{ backgroundColor: '#005B41', padding: 10, borderRadius: 20 }} onPress={toggleModal}>
-                    <MaterialIcons name="add" size={24} color="white" />
-                </TouchableOpacity>
-            </View>
-
-            {/* Chat-Liste */}
-            <FlatList
-                data={recentChats}
+      <View style={{ flex: 1, backgroundColor: '#232D3F' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding: 10, paddingTop: 51 }}>
+          <Text style={{ color: 'white', fontSize: 24 }}>Recent Chats</Text>
+          <TouchableOpacity style={{ backgroundColor: '#005B41', padding: 10, borderRadius: 20 }} onPress={toggleModal}>
+            <MaterialIcons name="add" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+  
+        {/* Suchfeld */}
+        <TextInput
+          ref={searchInputRef}
+          style={styles.searchInput}
+          placeholder="Suchen..."
+          placeholderTextColor="gray"
+          value={searchQuery}
+          onChangeText={(text) => setSearchQuery(text)}
+        />
+  
+        {/* Chat-Liste */}
+        <FlatList
+          data={filterData(recentChats)}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => navigation.navigate('ChatPage', { chatId: item.id })}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 17, borderBottomWidth: 1, borderColor: 'white' }}>
+                <View style={{ flexDirection: 'row' }}>
+                  <Text style={{ color: 'white', fontSize: 18 }}>{item.name}</Text>
+                  <Text style={{ color: 'white', fontSize: 14, marginLeft: 10 }}>{item.lastMessage}</Text>
+                </View>
+                <Text style={{ color: 'white', fontSize: 14 }}>{item.time}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+  
+        {/* Modal Overlay für Personenliste */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={toggleModal}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={{ color: '#DFDFDF', fontSize: 24 }}>Add Contact</Text>
+  
+              {/* Suchfeld für Kontakte */}
+              <TextInput
+                style={styles.searchInput}
+                placeholder="Suchen..."
+                placeholderTextColor="gray"
+                value={searchQuery}
+                onChangeText={(text) => setSearchQuery(text)}
+              />
+  
+              {/* Liste der verfügbaren Kontakte */}
+              <FlatList
+                data={filterData(availableContacts)}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('ChatPage', { chatId: item.id })}>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: 17, borderBottomWidth: 1, borderColor: 'white' }}>
-                            <View style={{ flexDirection: 'row' }}>
-                                <Text style={{ color: 'white', fontSize: 18 }}>{item.name}</Text>
-                                <Text style={{ color: 'white', fontSize: 14, marginLeft: 10 }}>{item.lastMessage}</Text>
-                            </View>
-                            <Text style={{ color: 'white', fontSize: 14 }}>{item.time}</Text>
-                        </View>
-                    </TouchableOpacity>
+                  <TouchableOpacity onPress={() => {
+                    toggleModal();
+                    navigation.navigate('ChatPage', { chatId: item.id })
+                  }}>
+                    <Text style={{ color: '#DFDFDF', fontSize: 18, paddingTop: 17 }}>{item.name}</Text>
+                  </TouchableOpacity>
                 )}
-            />
-
-            {/* Modal Overlay für Personenliste */}
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={isModalVisible}
-                onRequestClose={toggleModal}
-            >
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalContent}>
-                        <Text style={{ color: '#DFDFDF', fontSize: 24 }}>Add Contact</Text>
-                        <FlatList
-                            data={availableContacts}
-                            keyExtractor={(item) => item.id}
-                            renderItem={({ item }) => (
-                                <TouchableOpacity onPress={() => {
-                                    toggleModal();
-                                    navigation.navigate('ChatPage', { chatId: item.id })
-                                }}>
-                                    <Text style={{ color: '#DFDFDF', fontSize: 18, paddingTop: 17 }}>{item.name}</Text>
-                                </TouchableOpacity>
-                            )}
-                        />
-                        <TouchableOpacity onPress={toggleModal} style={{ marginTop: 10 }}>
-                            <FontAwesome name="close" size={24} color="red" />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal >
-        </View >
+              />
+              <TouchableOpacity onPress={toggleModal} style={{ marginTop: 10 }}>
+                <FontAwesome name="close" size={24} color="red" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+      </View>
     );
-}
-
-const styles = StyleSheet.create({
+  }
+  
+  const styles = StyleSheet.create({
     modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     modalContent: {
-        backgroundColor: '#232D3F',
-        padding: 50,
-        borderRadius: 10,
-        width: '80%',
-        marginVertical: 68,
+      backgroundColor: '#232D3F',
+      padding: 50,
+      borderRadius: 10,
+      width: '80%',
+      marginVertical: 68,
     },
-});
-
-export default FriendsPage;
+    searchInput: {
+      height: 40,
+      borderColor: 'white',
+      borderWidth: 1,
+      backgroundColor: 'white',
+      margin: 10,
+      paddingLeft: 10,
+      color: 'black',
+    },
+  });
+  
+  export default FriendsPage;
