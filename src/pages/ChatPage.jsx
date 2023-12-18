3c59f6ba53c1767b286f95404966cb0dcc9fc89d
LinusTimoDaniels <linustimo.daniels@gmail.com>
2023-12-18 14:16:47 +0100

    update

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 
 function ChatPage() {
+    const messagges = [
+        {sender: 'niel', time: new Date(), chatId: 5, text: 'hallo wie gehts?'},
+        {sender: 'linus', time: new Date(), chatId: 5, text: 'gut und dir?'},
+        {sender: 'sandro', time: new Date(), chatId: 5, text: 'mir gehts auch gut und euch'},
+    ]
+
+    const user = 'linus'
     return (
         <View>
             <Text>ChatPage</Text>
+            <FlatList data={messagges} renderItem={({item}) =>
+                <Text style={styles.item}>{item.sender}</Text>
+            }>
+            </FlatList>
         </View>
     )
 }
